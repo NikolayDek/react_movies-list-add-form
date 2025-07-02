c96cb8bd101d7a3b8eaa3b7dad03cfa311241261
Nikolay <diedaemon@gmail.com>
2025-07-02 21:30:32 +0200

    task complete

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { Movie } from './types/Movie';
 
 export const App = () => {
-  const [movies, setMovies] = useState(moviesFromServer);
+  const [movies, setMovies] = useState<Movie[]>(moviesFromServer);
 
   const addNewMovie = (movie: Movie) => {
     setMovies(currentMovies => [...currentMovies, movie]);
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -35,7 +35,7 @@ export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   };
 
   return (
-    <form className="NewMovie" key={count} onSubmit={handleOnAdd}>
+    <form className="NewMovie" onSubmit={handleOnAdd}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
